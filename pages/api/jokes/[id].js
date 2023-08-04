0e91b9c3c73b03e47a877f41b56d40dec2633108
ThomasF85 <thomas.foeldi@gmail.com>
2023-08-04 14:47:42 +0200

    add mongoose

--- pages/api/jokes/[id].js
+++ pages/api/jokes/[id].js
@@ -1,13 +1,20 @@
+import dbConnect from "@/db/connect.js";
 import { jokes } from "../../../lib/data.js";
+import Joke from "@/db/models/Joke.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
+  await dbConnect();
 
-  const joke = jokes.find((joke) => joke.id === id);
+  if (request.method === "GET") {
+    const joke = await Joke.findById(id);
 
-  if (!joke) {
-    return response.status(404).json({ status: "Not Found" });
+    if (!joke) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    return response.status(200).json(joke);
   }
 
-  response.status(200).json(joke);
+  response.status(405).json({ message: "Method not allowed" });
 }
